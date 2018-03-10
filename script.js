68312dc01a491a6709a6b0ecd388b195897714ea
Patrycja Kania <el.czupakabra21@gmail.com>
2018-03-10 19:45:14 +0100

    Add content to script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+
+var triangle1Area = getTriangleArea(2, 1);
+var triangle2Area = getTriangleArea(3, 8);
+var triangle3Area = getTriangleArea(5, 4);
+
+function getTriangleArea(a, h) {
+    if (isNaN(a, h) || a <= 0 || h <= 0) {
+    console.log('Dane nieprawidłowe');
+    return false;
+}
+    else {
+     return a*h/2;
+}
+}
+
+console.log(getTriangleArea(prompt('Base'), prompt('height')))
